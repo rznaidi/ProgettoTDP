30259cbcaf92092454002b5dd93d3d284b7ea5e0
sio <ale05faro@gmail.com>
2025-05-13 10:47:22 +0200

    Bottone colorato

---
+++ app/src/main/java/com/example/progettotdp/Register_activity.java
@@ -0,0 +1,14 @@
+package com.example.progettotdp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_activity);
+    }
+}
